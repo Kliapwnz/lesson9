b6321b1c5045eb544d8b9f3ca70f3ff54dc37728
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-28 18:38:37 +0300

    up data beforeEach

--- src/state/todolists-reducer.test.tsx
+++ src/state/todolists-reducer.test.tsx
@@ -9,14 +9,19 @@ import {
 import {v1} from 'uuid';
 import {FilterValuesType, TodolistType} from '../App';
 
-let todolistId1 = v1();
-let todolistId2 = v1();
+let todolistId1: string
+let todolistId2: string
 
-const startState: Array<TodolistType> = [
-    {id: todolistId1, title: "What to learn", filter: "all"},
-    {id: todolistId2, title: "What to buy", filter: "all"}
-]
+let startState: Array<TodolistType>
+beforeEach(() => {
+    todolistId1 = v1();
+    todolistId2 = v1();
 
+    startState = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+})
 
 test('correct todolist should be removed', () => {
 
